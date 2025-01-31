bd5723e2e864727cd9b2d0af73a17848ad7d38c8
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-31 22:12:21 +0100

    fixed font issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
 
     <>
-      <h1 className='text-red-500 font-circularweb'>Hello</h1>
+      <h1 className='text-red-500 font-general'>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat iste, illo eveniet perferendis cum molestiae eum pariatur, enim odit dignissimos omnis, sint vitae tempora unde ea recusandae debitis beatae ducimus.</h1>
     </>
   )
 }
