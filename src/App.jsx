87703572461fb4f88a1568379717143ddcb72bf4
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-02-01 21:31:13 +0100

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './index.css'
-
+import Hero from './components/Hero';
 function App() {
-
-
   return (
-
     <>
-      <h1 className='text-red-500 font-general'>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat iste, illo eveniet perferendis cum molestiae eum pariatur, enim odit dignissimos omnis, sint vitae tempora unde ea recusandae debitis beatae ducimus.</h1>
+      <main className='relative min-h-screen w-screen overflow-x-hidden'>
+        <Hero />
+      </main>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+const Hero = () => {
+    return (
+        <div className="relative h-dvh w-screen">
+            <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+                <div className="mask-clip-path">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
